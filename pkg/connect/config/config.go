f8672224b5b62efa4fa8b6ceb3b0ae80922fe34b
Kevin Joiner <10265309+KevinJoiner@users.noreply.github.com>
2025-09-22 05:36:43 -0400

    feat: add timeout config

--- pkg/connect/config/config.go
+++ pkg/connect/config/config.go
@@ -13,11 +13,25 @@ type Settings struct {
 	User     string `env:"CLICKHOUSE_USER"     yaml:"CLICKHOUSE_USER"`
 	Password string `env:"CLICKHOUSE_PASSWORD" yaml:"CLICKHOUSE_PASSWORD"`
 	Database string `env:"CLICKHOUSE_DATABASE" yaml:"CLICKHOUSE_DATABASE"`
+	// ReadTimeout is the timeout for reading from the clickhouse database.
+	// defaults to 5ms
+	ReadTimeout string `env:"CLICKHOUSE_READ_TIMEOUT" yaml:"CLICKHOUSE_READ_TIMEOUT"`
+	// DialTimeout is the timeout for dialing the clickhouse database.
+	// defaults to 30s
+	DialTimeout string `env:"CLICKHOUSE_DIAL_TIMEOUT" yaml:"CLICKHOUSE_DIAL_TIMEOUT"`
 
 	RootCAs *x509.CertPool `env:"-" yaml:"-"`
 }
 
 // DSN returns the Data Source Name (DSN) for connecting to ClickHouse.
 func (s Settings) DSN() string {
-	return fmt.Sprintf("clickhouse://%s:%d/%s?username=%s&password=%s&secure=true&dial_timeout=200ms&max_execution_time=60", s.Host, s.Port, s.Database, url.QueryEscape(s.User), url.QueryEscape(s.Password))
+	dialTimeout := s.DialTimeout
+	if dialTimeout == "" {
+		dialTimeout = "200ms"
+	}
+	readTimeout := s.ReadTimeout
+	if readTimeout == "" {
+		readTimeout = "5m"
+	}
+	return fmt.Sprintf("clickhouse://%s:%d/%s?username=%s&password=%s&secure=true&dial_timeout=%s&read_timeout=%s&max_execution_time=60", s.Host, s.Port, s.Database, url.QueryEscape(s.User), url.QueryEscape(s.Password), dialTimeout, readTimeout)
 }
--- pkg/connect/connect.go
+++ pkg/connect/connect.go
@@ -10,8 +10,27 @@ import (
 	"github.com/DIMO-Network/clickhouse-infra/pkg/connect/config"
 )
 
+const (
+	defaultDialTimeout = time.Second * 30
+	defaultReadTimeout = time.Minute * 5
+)
+
 // GetClickhouseDB returns a sql.DB connection to clickhouse.
 func GetClickhouseDB(settings *config.Settings) *sql.DB {
+	dialTimeout := defaultDialTimeout
+	if settings.DialTimeout == "" {
+		sDialTimeout, err := time.ParseDuration(settings.DialTimeout)
+		if err == nil {
+			dialTimeout = sDialTimeout
+		}
+	}
+	readTimeout := defaultReadTimeout
+	if settings.ReadTimeout == "" {
+		sReadTimeout, err := time.ParseDuration(settings.ReadTimeout)
+		if err == nil {
+			readTimeout = sReadTimeout
+		}
+	}
 	addr := fmt.Sprintf("%s:%d", settings.Host, settings.Port)
 	conn := clickhouse.OpenDB(&clickhouse.Options{
 		Addr: []string{addr},
@@ -20,7 +39,8 @@ func GetClickhouseDB(settings *config.Settings) *sql.DB {
 			Password: settings.Password,
 			Database: settings.Database,
 		},
-		DialTimeout: time.Minute * 30,
+		DialTimeout: dialTimeout,
+		ReadTimeout: readTimeout,
 		TLS: &tls.Config{
 			MinVersion: tls.VersionTLS12,
 			RootCAs:    settings.RootCAs,
@@ -31,6 +51,22 @@ func GetClickhouseDB(settings *config.Settings) *sql.DB {
 
 // GetClickhouseConn returns a clickhouse.Conn connection to clickhouse.
 func GetClickhouseConn(settings *config.Settings) (clickhouse.Conn, error) {
+	dialTimeout := defaultDialTimeout
+	if settings.DialTimeout == "" {
+		var err error
+		dialTimeout, err = time.ParseDuration(settings.DialTimeout)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse dial timeout: %w", err)
+		}
+	}
+	readTimeout := defaultReadTimeout
+	if settings.ReadTimeout == "" {
+		var err error
+		readTimeout, err = time.ParseDuration(settings.ReadTimeout)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse read timeout: %w", err)
+		}
+	}
 	addr := fmt.Sprintf("%s:%d", settings.Host, settings.Port)
 	conn, err := clickhouse.Open(&clickhouse.Options{
 		Addr: []string{addr},
@@ -39,7 +75,8 @@ func GetClickhouseConn(settings *config.Settings) (clickhouse.Conn, error) {
 			Password: settings.Password,
 			Database: settings.Database,
 		},
-		DialTimeout: time.Minute * 30,
+		DialTimeout: dialTimeout,
+		ReadTimeout: readTimeout,
 		TLS: &tls.Config{
 			MinVersion: tls.VersionTLS12,
 			RootCAs:    settings.RootCAs,
